0529e2e96921445f56cfd970c2322ae72cd85fab
Boris027 <titankiller027@gmail.com>
2023-11-11 01:29:16 +0100

    entrega 1

---
+++ js/proyectolibre.js
@@ -0,0 +1,17 @@
+function mostrar(element){
+    document.getElementById(element).style.visibility="visible"; 
+    
+    
+}
+function ocultar(element){
+    document.getElementById(element).style.visibility="hidden";
+}
+function overflow(){
+    var body = document.body;
+    body.style.overflow = 'hidden';
+}
+
+function overflowexit(){
+    var body = document.body;
+    body.style.overflow = 'scroll';
+}
