29cdeb34d54f588c2efa6a341d171970d0019469
Boris027 <titankiller027@gmail.com>
2023-12-02 18:52:56 +0100

    subida menú

--- js/proyectolibre.js
+++ js/proyectolibre.js
@@ -15,3 +15,24 @@ function overflowexit(){
     var body = document.body;
     body.style.overflow = 'scroll';
 }
+
+
+
+
+
+
+function openLeftMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+  }
+  
+  function closeLeftMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+  }
+  
+  function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+  }
+  
+  function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+  }
