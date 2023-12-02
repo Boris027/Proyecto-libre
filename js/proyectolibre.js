d2c29685aa4d64dfda3315fefd3ae3eb75d09f7b
Boris027 <titankiller027@gmail.com>
2023-12-02 21:29:15 +0100

    subida scroll

--- js/proyectolibre.js
+++ js/proyectolibre.js
@@ -21,18 +21,3 @@ function overflowexit(){
 
 
 
-function openLeftMenu() {
-    document.getElementById("leftMenu").style.display = "block";
-  }
-  
-  function closeLeftMenu() {
-    document.getElementById("leftMenu").style.display = "none";
-  }
-  
-  function openRightMenu() {
-    document.getElementById("rightMenu").style.display = "block";
-  }
-  
-  function closeRightMenu() {
-    document.getElementById("rightMenu").style.display = "none";
-  }
